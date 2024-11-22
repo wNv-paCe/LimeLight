533453c2314a240ac03680e8b62e6898b80741c9
wNv-paCe <wnv.pace@gmail.com>
2024-11-22 03:03:35 -0700

    First submit - frame

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-3xl font-pblack">Lime Light</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
