2b3dbdacdf6b7cdc961b1a81760ba59cb05dd117
wNv-paCe <wnv.pace@gmail.com>
2024-12-11 13:38:50 -0700

    modified style

--- app/(tabs)/settings.js
+++ app/(tabs)/settings.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, Text } from "react-native";
+import { View, Text, SafeAreaView } from "react-native";
 import UnitSelector from "../components/UnitSelector";
 import DefaultCitySetting from "../components/DefaultCitySetting";
 
@@ -8,17 +8,20 @@ export default function Settings() {
   const [defaultCity, setDefaultCity] = useState("Calgary");
 
   return (
-    <View className="flex-1 bg-blue-500/80">
-      <View className="p-8 mt-20">
-        <Text className="text-3xl font-psemibold mb-8 text-white">Setting</Text>
+    <SafeAreaView className="flex-1 bg-blue-500/80">
+      {/* 标题部分 */}
+      <View className="p-8 mt-10">
+        <Text className="text-3xl font-psemibold text-gray-900">Setting</Text>
       </View>
-      <View className="p-8">
+
+      {/* 设置选项部分 */}
+      <View className="flex-1 p-8">
         <DefaultCitySetting
           defaultCity={defaultCity}
           setDefaultCity={setDefaultCity}
         />
         <UnitSelector tempUnit={tempUnit} setTempUnit={setTempUnit} />
       </View>
-    </View>
+    </SafeAreaView>
   );
 }
